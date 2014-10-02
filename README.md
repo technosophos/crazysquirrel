6a5ed816b76c2676e943d0a9177042cc14e6b16c
Matt Butcher <technosophos@gmail.com>
2014-10-01 21:10:41 -0600

    Initial add.

---
+++ README.md
@@ -0,0 +1,83 @@
+# CrazySquirrel: Squirrel wrappers for Cookoo
+
+This library provides hooks for using the Squirrel library from within
+Cookoo.
+
+[Cookoo](https://github.com/Masterminds/cookoo) is a chain-of-command
+application building framework written in Go.
+
+[Squirrel](https://github.com/lann/squirrel) is a database query
+building library that is both powerful and easy to use.
+
+## Installation
+
+```
+$ go get github.com/technosophos/crazysquirrel/db
+```
+
+Or, for [glide](https://github.com/Masterminds/glide):
+
+```
+package: your/package/name
+import:
+  - package: github.com/Masterminds/cookoo
+  - package: github.com/lann/builder
+  - package: github.com/lann/squirrel
+  - package: github.com/technosophos/crazysquirrel
+  # ...
+
+```
+
+## Usage
+
+The main purpose of this library is to make it trivially easy to use the
+following workflow:
+
+1. Set up a database connection early in your program.
+2. Access the database from anywhere that the cookoo.Context is
+   available.
+3. Provide transparent and simple access to the Squirrel builder and
+   Proxy/Runner.
+
+Below is an extended example.
+
+```go
+	package main
+
+	import (
+		"github.com/Masterminds/cookoo"
+		"github.com/technosophos/crazysquirrel/db"
+		"database/sql"
+	)
+
+	func main() {
+		reg, router, cxt := cookoo.Cookoo()
+
+		conn, err := sql.Open("postgres", "something")
+		// ...
+
+		// Set up a Postgres-friendly Squirrel environment.
+		db.SetupDatasource(cxt, conn, db.Postgres)
+	}
+
+	func MyCommand(c cookoo.Context, p *cookoo.Params) (interface{}, cookoo.Interrupt) {
+		// Now we can use the database from anywhere that has a Context.
+
+		rows, err := db.Builder(c).Select("*").From("MyTable").Query()
+
+		// ...
+	}
+
+	func Example(c cookoo.Context, p *cookoo.Params) (interface{}, cookoo.Interrupt) {
+		// Get the *sql.DB
+		conn := db.Db(c)
+
+		// Get a squirrel.DbProxy (which happens to also be a squirrel.BaseRunner)
+		// This implements most of the operations that are on *sql.Db, but
+		// it wraps them in the statement cache.
+		cache := db.Runner(c)
+
+		// Get a squirrel.Builder
+		builder := db.Builder(c)
+	}
+```
---
+++ db/datasource.go
@@ -0,0 +1,127 @@
+/* Package db provides database helpers for using Squirrel with Cookoo.
+
+This package provides a few convenience tools for consistently working with
+Squirrel-based database connections in Cookoo.
+
+Usage:
+
+	package main
+
+	import (
+		"github.com/Masterminds/cookoo"
+		"github.com/technosophos/crazysquirrel/db"
+		"database/sql"
+	)
+
+	func main() {
+		reg, router, cxt := cookoo.Cookoo()
+
+		conn, err := sql.Open("postgres", "something")
+		// ...
+
+		// Set up a Postgres-friendly Squirrel environment.
+		db.SetupDatasource(cxt, conn, db.Postgres)
+	}
+
+	func MyCommand(c cookoo.Context, p *cookoo.Params) (interface{}, cookoo.Interrupt) {
+		// Now we can use the database from anywhere that has a Context.
+
+		rows, err := db.Builder(c).Select("*").From("MyTable").Query()
+
+		// ...
+	}
+
+	func Example(c cookoo.Context, p *cookoo.Params) (interface{}, cookoo.Interrupt) {
+		// Get the *sql.DB
+		conn := db.Db(c)
+
+		// Get a squirrel.DbProxy (which happens to also be a squirrel.BaseRunner)
+		// This implements most of the operations that are on *sql.Db, but
+		// it wraps them in the statement cache.
+		cache := db.Runner(c)
+
+		// Get a squirrel.Builder
+		builder := db.Builder(c)
+	}
+
+*/
+package db
+
+import (
+	"github.com/Masterminds/cookoo"
+	"github.com/lann/squirrel"
+	"database/sql"
+)
+
+const (
+	// Key for the raw *sql.Db
+	DbObj = "db.Obj"
+	// Key for getting the Squirrel StatementCacheProxy.
+	DbRunner = "db.Runner"
+	// Key for getting the Squirrel StatementBuilder.
+	DbBuilder = "db.Builder"
+)
+
+// Config provides configuration options to SetupDatasource.
+//
+// Predefined configurations for Postgres and MySQL are provided as variables.
+type Config struct {
+	Placeholder squirrel.PlaceholderFormat
+}
+
+// Predefined config for Postgres defaults
+var Postgres = Config{Placeholder: squirrel.Dollar}
+
+// Predifined config for MySQL defaults
+var MySQL = Config{Placeholder: squirrel.Question}
+
+// SetupDatasource takes a database connection and creates an easy-to-use
+// Squirrel wrapper.
+//
+// Generally, this is called early in Cookoo's initialization. See the example
+// in the package documentation.
+//
+// The Config struct is used to pass options into the setup. Default
+// configurations are provided for Postgres and MySQL.
+//
+// Example:
+//
+// 	db.SetupDatasource(c, dbConn, db.MySQL)
+func SetupDatasource(c cookoo.Context, db *sql.DB, cfg Config) {
+
+	// Statement Cache is a cache for prepared statements.
+	sc := squirrel.NewStmtCacheProxy(db)
+	builder := squirrel.StatementBuilder.RunWith(sc)
+
+	if cfg.Placeholder != nil {
+		builder.PlaceholderFormat(cfg.Placeholder)
+	}
+
+	c.AddDatasource(DbObj, db)
+	c.AddDatasource(DbRunner, sc)
+	c.AddDatasource(DbBuilder, sc)
+}
+
+// Builder fetches the Squirrel statement builder datasource.
+//
+// Usage:
+//
+// 	db.Builder(cxt).Select("*").From("foo").Query()
+//
+func Builder(c cookoo.Context) *squirrel.StatementBuilderType {
+	return c.Datasource(DbBuilder).(*squirrel.StatementBuilderType)
+}
+
+// Runner fetches a Squirrel DBProxyBeginner datasource.
+//
+// Usage:
+//
+// 	db.Runner(cxt).Exec("SELECT * FROM foo LIMIT 10")
+func Runner(c cookoo.Context) squirrel.DBProxyBeginner {
+	return c.Datasource(DbRunner).(squirrel.DBProxyBeginner)
+}
+
+// Db fetches the *sql.DB datasource.
+func Db(c cookoo.Context) *sql.DB {
+	return c.Datasource(DbObj).(*sql.DB)
+}
